9e30f845c6d04f859528be3a52f0acdaf849dcc8
VolandLee <83191094+VolandLee@users.noreply.github.com>
2024-06-24 04:20:40 +0500

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,13 +1,16 @@
 #Master node 
 ARBITER_HOST="192.168.187.130"
 SLAVE_HOST="192.168.187.129"
+LOG_FILE="./master.log"
 
 while true; do
     ARBITER_STATUS=$(pg_isready -h $ARBITER_HOST)
     SLAVE_STATUS=$(pg_isready -h $SLAVE_HOST)
+    echo "$(date): Arbiter status: $ARBITER_STATUS" >> $LOG_FILE
+    echo "$(date): Slave status: $SLAVE_STATUS" >> $LOG_FILE
 
     if [[ $ARBITER_STATUS != *"accepting connections"* ]] && [[ $SLAVE_STATUS != *"accepting connections"* ]]; then
-        echo "$(date): Потеряна связь с остальными узлами. Перезапуск Master."
+        echo "$(date): Потеряна связь с остальными узлами. Перезапуск Master." >> $LOG_FILE
         sudo systemctl restart postgresql-12
     fi
 
