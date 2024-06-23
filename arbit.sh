df2a1dfb8c8b9ddbaeadc4aa612356959d9fddf4
VolandLee <83191094+VolandLee@users.noreply.github.com>
2024-06-24 04:19:50 +0500

    Update arbit.sh

--- arbit.sh
+++ arbit.sh
@@ -3,10 +3,12 @@
 MASTER_HOST="192.168.187.128"
 SLAVE_HOST="192.168.187.129"
 PORT=8080
-
+LOG_FILE="./arbiter.log"
 check_connections() {
     MASTER_STATUS=$(pg_isready -h $MASTER_HOST)
     SLAVE_STATUS=$(pg_isready -h $SLAVE_HOST)
+    echo "$(date): Master status: $MASTER_STATUS" >> $LOG_FILE
+    echo "$(date): Slave status: $SLAVE_STATUS" >> $LOG_FILE
 }
 
 handle_http_requests() {
@@ -17,8 +19,10 @@ handle_http_requests() {
                 if [[ $MASTER_STATUS != *"accepting connections"* ]]; then
 					#Ответ в случае отсутствия соединения
                     RESPONSE="HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n"
+		    echo "$(date): Sending 500 response to /check_master request" >> $LOG_FILE
                 else
                     RESPONSE="HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"
+		    echo "$(date): Sending 200 response to /check_master request" >> $LOG_FILE
                 fi
                 echo -e $RESPONSE
             fi
@@ -33,7 +37,7 @@ while true; do
     check_connections
 	#Случай когда все узлы недоступны
     if [[ $MASTER_STATUS != *"accepting connections"* ]] && [[ $SLAVE_STATUS != *"accepting connections"* ]]; then
-        echo "$(date): Потеряна связь с другими узлами. Перезапуск Арбитра"
+        echo "$(date): Потеряна связь с другими узлами. Перезапуск Арбитра" >> $LOG_FILE
         sudo systemctl restart postgresql-12
     fi
 
